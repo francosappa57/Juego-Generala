66456ae129aec3c7ff3781214d527a81f60d4064
Franco Sappa <sappa57@gmail.com>
2025-11-04 18:05:46 -0300

    feat: Update Juego generala

--- funciones.py
+++ funciones.py
@@ -1,34 +1,47 @@
 import random
 
 def jugar():
-    #for rondas in range(1):
-        print("\nplantilla")
-        # guardados = guardar_dados()
-        # for turnos in range(3):
-        #     print(f"\nTurno {turnos + 1}")
-        tirada()
+    guardados = []
+
+    print("\nPLANTILLA")
+    for turnos in range(3):
+        print(f"\nTurno {turnos + 1}")
+        jugada = tirada(guardados)
+        if turnos == 2:
+            pass
+        else:
+            valor_dados = guardar_dados(jugada)    
+            for i in valor_dados:
+                guardados.append(i)
+        print(guardados)
 
 def tirada(lista):
     lista_dados = []
+    dados = 5
     inicio_caras = 1
     fin_caras = 6
-    for turnos in range(3):
-        print(f"\nTurno {turnos + 1}")
-        for _ in range(5):
-            dado = random.randint(inicio_caras, fin_caras)
-            lista_dados.append(dado)
-        print(lista_dados)
-        guardar_dados(lista_dados)
+    # if len(lista) == 0:
+    for _ in range(dados - len(lista)):
+        valor = random.randint(inicio_caras, fin_caras)
+        lista_dados.append(valor)
+    # else:
+    #     for _ in range(dados - len(lista)):
+    #         valor = random.randint(inicio_caras, fin_caras)
+    #         lista_dados.append(valor)
+    print(lista_dados)
+    return lista_dados    
 
 
 def guardar_dados(lista):
     dados_guardados = []
     
     pedido = input("Elige la posicion de los dados a guardar: ").split(',')
-    
-    for i in range(len(lista)):
-        for x in pedido:
-            if int(x) == i:
-                dados_guardados.append(lista[i])
-    print(dados_guardados)
+
+    if pedido == "":
+        pass
+    else:
+        for i in range(len(lista)):
+            for x in pedido:
+                if int(x) == i:
+                    dados_guardados.append(lista[i])
     return dados_guardados
