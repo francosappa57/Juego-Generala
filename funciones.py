87f859944dc0961c592857bad12a87968b7e57cf
Franco Sappa <sappa57@gmail.com>
2025-11-05 17:46:44 -0300

    feat: Solucion al problema de mas de 5 elmentos en las lista y evitar que el usuario solicite guardar dados en el turno 3

--- funciones.py
+++ funciones.py
@@ -1,7 +1,8 @@
 import random
 
 def jugar():
-    for rondas in range(2):
+    contador = 1
+    for rondas in range(contador):
         guardar_jugadas = []
     
         print("\n" + "-"*26)
@@ -17,56 +18,49 @@ def jugar():
         print(f"Total {'-':>16}")
         print("-"*26)
         
+        print(f"\n=== RONDA {rondas + 1} - Rondas restantes: {contador - rondas - 1} ===")
         for turnos in range(3):
             print(f"\nTurno {turnos + 1}")
-            jugada = tirada(guardar_jugadas)
+            tirada(guardar_jugadas)
             if turnos != 2:
-                valor_dados = guardar_dados(jugada)
-                if len(valor_dados) != 0:    
-                    for i in valor_dados:
-                        guardar_jugadas.append(i)
-            
+                guardar_jugadas = guardar_dados(guardar_jugadas)
+        
             if len(guardar_jugadas) == 5:
                 break
         calcular_jugada(guardar_jugadas)
-    #       print(guardados)
+
 
 def tirada(lista):
-    lista_dados = []
-    if len(lista) > 0:
-        for v in lista:
-            lista_dados.append(v)
     dados = 5
     inicio_caras = 1
     fin_caras = 6
     for _ in range(dados - len(lista)):
         valor = random.randint(inicio_caras, fin_caras)
-        lista_dados.append(valor)
-    print(lista_dados)
-    return lista_dados    
+        lista.append(valor)
+    print(lista)
+    return   
 
 
-def guardar_dados(lista):
+def guardar_dados(jugada):
     dados_guardados = []
-
-    pedido = input("Elige la posicion de los dados a guardar: ")
-
+    
+    pedido = input("Elige la posicion de los dados a guardar: ").strip()
     if pedido == "":
         return dados_guardados
-    else:
-        for i in range(len(lista)):
-            for x in pedido.split(','):
-                if int(x) == i + 1:
-                    dados_guardados.append(lista[i])
+    
+    for x in pedido.split(','):
+        dados_guardados.append(jugada[int(x) - 1])
     return dados_guardados
+    
 
 
 def calcular_jugada(lista):
     jugadas = ("1","2","3","4","5","6","Escalera","Full","Poker","Generala")
+    lista.sort()
     
+    print(lista)
+        
     
-    print("\n--- Jugadas disponibles ---")
-    for i in range(len(jugadas)):
-        print(f"[{i + 1}] - {jugadas[i]}: -")   
-    
-    pass
+    # print("\n--- Jugadas disponibles ---")
+    # for i in range(len(jugadas)):
+    #     print(f"[{i + 1}] - {jugadas[i]}: -")
